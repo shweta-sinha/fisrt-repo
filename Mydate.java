ec26bf8e3265716a07a171d89929cd79e4067597
shweta-sinha <75249015+shweta-sinha@users.noreply.github.com>
2020-12-09 16:47:07 +0530

    Create Mydate.java

---
+++ Mydate.java
@@ -0,0 +1,53 @@
+public class Mydate {
+	int year;
+	int date;
+	int month;
+	public Mydate()
+	{
+		year=2020;
+		month=12;
+		date=17;
+		
+	}
+	public Mydate(int date,int month,int year)
+	{
+		this.year=year;
+		this.month=month;
+		this.date=date;
+		
+	}
+	
+	public void display()
+	{
+		System.out.println(date+"/"+month+"/"+year);
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "" + year + "/" + date + "/" + month + "";
+	}
+	public void set(int date)
+	{
+		this.date=date;
+		
+		
+	}
+	public int get()
+	{
+		return date;
+		
+	}
+	public static void main(String[] args) {
+		Mydate obj=new Mydate();
+		
+		
+		
+		
+		Mydate obj2=new Mydate(9,12,2020);
+		obj2.display();
+	    
+
+	}
+
+}
